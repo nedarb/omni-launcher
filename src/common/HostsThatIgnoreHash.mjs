9193154e093bef249417ac12ca9fb1c243879db0
Braden Hays <whays@salesforce.com>
2024-03-25 11:20:09 -0700

    Adding helper function

--- src/common/HostsThatIgnoreHash.mjs
+++ src/common/HostsThatIgnoreHash.mjs
@@ -26,4 +26,25 @@ const facade = {
     return (await storage.get(key)) ?? [];
   }
 };
+
+/**
+ * @typedef {any} T
+ * @param {Array<T>} collection 
+ * @param {keyof T} urlKey
+ * @returns {Promise<Map<string, Array<T>>>}
+ */
+export async function getGroupedByUrl(collection, urlKey) {
+  const hosts = await facade.getHosts();
+
+  return collection.reduce((map, item)=> {
+    const url = item[urlKey];
+    const urlObj = url ? new URL(url) : null;
+    const shouldIgnoreHash = !!hosts.find(host=>urlObj.host.endsWith(host));
+    const key = shouldIgnoreHash ? urlObj.origin + urlObj.pathname : url;
+    const existing = map.get(key) || [];
+    map.set(key, [...existing, item]);
+    return map;
+  }, new Map());
+}
+
 export default facade;
